a0c305f76245e1153c942c2f37f9286ff804a769
Saravanan Ganesh <srrvnn@gmail.com>
2019-05-06 16:19:30 +0100

    move use-cors to the top of the configuraiton

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
