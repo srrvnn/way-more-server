bcd37929c66e2196d0ec17a360f17030236f4f70
Saravanan Ganesh <srrvnn@gmail.com>
2019-05-06 13:25:10 +0100

    hacky api endpoints for POC

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer = require('multer');
+var cors = require('cors')
+var fs = require('fs');
+var os = require('os');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public')
+  },
+  filename: function (req, file, cb) {
+    fileExtension = file.originalname.split(/[. ]+/).pop();
+    if (['jpg', 'png'].includes(fileExtension)) {
+      cb(null, 'corpus' + Date.now() + '.' + fileExtension);
+    }
+  }
+})
+var upload = multer({ storage: storage });
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -12,9 +27,67 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.post('/upload', upload.single('fileKey'), function (req, res, next) {
+  res.json({ "msg": "saved " + req.file.filename });
+});
+
+app.post('/save', function (req, res, next) {
+  var label = req.body.image.replace(getHostname(req), '') + ' ' + req.body.food + ' ' + req.body.spicy + os.EOL;
+  fs.appendFile('label.txt', label, function (err) {
+    if (err) throw err;
+    console.log('Updated labels!');
+  });
+  res.json({ "msg": "saved"});
+});
+
+app.get('/train', function (req, res) {
+  var itemsResponse = {items : []};
+  var labels = '';
+  try {
+    labels = fs.readFileSync('label.txt', 'utf8');
+  } catch (err) {
+    console.log('labels file does not exist');
+  }
+  fs.readdirSync('public/').forEach(file => {
+    if (file.includes('corpus') && !labels.includes(file)) {
+      itemsResponse.items.push({
+        train: true,
+        image: getHostname(req) + file,
+        food: null,
+        spicy: null,
+      });
+    }
+  });
+  res.json(itemsResponse);
+});
+
+app.get('/corpus', function (req, res) {
+  var total = 0;
+  var trained = 0; 
+  var labels = '';
+  try {
+    labels = fs.readFileSync('label.txt', 'utf8');
+  } catch (err) {
+    console.log('labels file does not exist');
+  }
+  fs.readdirSync('public/').forEach(file => {
+    if (file.includes('corpus')) {
+      total++;
+      if (labels.includes(file)) {
+        trained++;
+      }
+    }
+  });
+  res.json({total: total, trained: trained, trained_ratio: trained/total, untrained: total - trained, untrained_ratio: (total - trained)/total });
+});
+
+function getHostname(req) {
+  return req.protocol + '://' + req.get('host') + '/';
+}
 
 module.exports = app;
